8ea744ab27b0f826e5ea83becad0d39e41a6a422
Mihai <litamihai13@gmail.com>
2022-09-01 10:17:58 +0300

    Added another project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,9 @@ import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
-import IMG5 from '../../assets/portfolio5.png'
+import IMG5 from '../../assets/portfolio5.jpeg'
 import IMG6 from '../../assets/portfolio6.png'
+import IMG7 from '../../assets/portfolio7.png'
 
 const data = [
   {
@@ -13,41 +14,49 @@ const data = [
     image: IMG1,
     title: "Club Capybara",
     github: 'https://github.com/LitaMihai/Club-Capybara',
-    demo: ''
+    demo: 'https://github.com/LitaMihai/Club-Capybara'
   },
   {
     id: 2,
     image: IMG2,
     title: "Car Rental Application",
     github: 'https://github.com/LitaMihai/Car_Rental',
-    demo: ''
+    demo: 'https://github.com/LitaMihai/Car_Rental'
   },
   {
     id: 3,
     image: IMG3,
     title: "Java Calculator",
     github: 'https://github.com/LitaMihai/Java_Calculator',
-    demo: ''},
+    demo: 'https://github.com/LitaMihai/Java_Calculator'
+  },
   {
     id: 4,
     image: IMG4,
     title: "Lords of Space",
     github: 'https://github.com/LitaMihai/Lords-of-Space',
-    demo: ''
+    demo: 'https://github.com/LitaMihai/Lords-of-Space'
   },
   {
     id: 5,
     image: IMG5,
-    title: "Destroy The Enemies",
-    github: 'https://github.com/LitaMihai/Destroy_The_Enemies',
-    demo: ''
+    title: "Eat as much as you can",
+    github: 'https://github.com/LitaMihai/Eat_as_much_as_you_can',
+    demo: 'https://github.com/LitaMihai/Eat_as_much_as_you_can'
   },
   {
     id: 6,
     image: IMG6,
+    title: "Destroy The Enemies",
+    github: 'https://github.com/LitaMihai/Destroy_The_Enemies',
+    demo: 'https://github.com/LitaMihai/Destroy_The_Enemies'
+  },
+  {
+    id: 7,
+    image: IMG7,
     title: "Snake Game",
     github: 'https://github.com/LitaMihai/Snake-Game',
-    demo: ''
+    demo: 'https://github.com/LitaMihai/Snake-Game'
   }
 ]
 
@@ -67,8 +76,8 @@ const Portfolio = () => {
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-                <a href={github} className='btn' target='_blank'>Github</a>
-                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
               </div>
             </article>
           )
